a1252bca6d4facdae0a61bb73c7b60a9756875a6
devbathani <bathanid888@gmail.com>
2023-06-30 15:17:39 +0530

    OTPLess Vue Js Demo code

---
+++ otpless_vue_js_project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/otpless_icon.png" />
+</template>
+
+<script>
+export default {
+  /************************************* */
+  // This is the main function that will be called when the user clicks on the button
+  /************************************* */
+  mounted() {
+    window.otpless = (otplessUser) => {
+      alert(JSON.stringify(otplessUser));
+    };
+  },
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 350px;
+}
+</style>
