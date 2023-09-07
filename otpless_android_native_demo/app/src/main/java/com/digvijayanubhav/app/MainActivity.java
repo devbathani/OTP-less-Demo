438422a2de0e727434bf3c66ded28f7e8b61f4af
devbathani <bathanid888@gmail.com>
2023-09-07 15:57:44 +0530

    Base code to 2.1.6

--- otpless_android_native_demo/app/src/main/java/com/digvijayanubhav/app/MainActivity.java
+++ otpless_android_native_demo/app/src/main/java/com/digvijayanubhav/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.digvijayanubhav.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,55 +12,72 @@ import android.widget.Toast;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.otpless.views.OtplessManager;
+import com.otpless.dto.OtplessResponse;
+import com.otpless.main.OtplessManager;
+import com.otpless.main.OtplessView;
 
 import org.json.JSONObject;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
+    OtplessView otplessView;
+    TextView userName ;
+    TextView userNumber ;
+    private void onOtplessCallback(OtplessResponse response) {
+        if (response.getErrorMessage() != null) {
+            // todo error handing
+        } else {
+            final String token = response.getData().optString("token");
+            final JSONObject otplessUser = response.getData();
+            Toast.makeText(MainActivity.this, otplessUser.toString(), Toast.LENGTH_SHORT).show();
+            String mobile=otplessUser.optString("mobile");
+            String mobileName=null;
+            String mobileNumber=null;
+            try {
+                HashMap<String,String> mobileHash= new ObjectMapper().readValue(mobile,HashMap.class);
+                mobileName=mobileHash.get("name");
+                mobileNumber=mobileHash.get("number");
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            userName.setText(mobileName);
+            userNumber.setText(mobileNumber);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         userName = findViewById(R.id.userName);
+         userNumber = findViewById(R.id.userNumber);
+        // Initialise OtplessView
+        otplessView = OtplessManager.getInstance().getOtplessView(this);
+        otplessView.setCallback(this::onOtplessCallback, null, false);
+        // calling startOtpless method is optional to call here, we can also call it on buttons click
 
-        OtplessManager.getInstance().init(this);
-        OtplessManager.getInstance().showFabButton(false);
         Button loginButton = findViewById(R.id.loginButton);
-        TextView userName = findViewById(R.id.userName);
-        TextView userNumber = findViewById(R.id.userNumber);
-     
         //This function will be used to trigger the OTPless floater
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                OtplessManager.getInstance().start(data -> {
-                    if (data.getData() == null) {
-                        Log.e("OTP-less", data.getErrorMessage());
-                    } else {
-                        final JSONObject otplessUser = data.getData();
-                        Toast.makeText(MainActivity.this, otplessUser.toString(), Toast.LENGTH_SHORT).show();
-                        String mobile=otplessUser.optString("mobile");
-                        String mobileName=null;
-                        String mobileNumber=null;
-                        try {
-                            HashMap<String,String> mobileHash= new ObjectMapper().readValue(mobile,HashMap.class);
-                            mobileName=mobileHash.get("name");
-                            mobileNumber=mobileHash.get("number");
-                        } catch (JsonProcessingException e) {
-                            e.printStackTrace();
-                        }
-                        userName.setText(mobileName);
-                        userNumber.setText(mobileNumber);
-                        final String token = otplessUser.optString("token");
-                        if (!token.isEmpty()) {
-                            Log.d("OTP-less", String.format("token: %s", token));
-                            // todo send this token to your backend service to validate otplessUser details received in the callback with OTPless backend service
-                        }
-                    }
-                });
+                otplessView.showOtplessLoginPage();
+                // very important to call here, verification is done on low memory recreate case
+                otplessView.verifyIntent(getIntent());
             }
         });
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (otplessView != null) {
+            otplessView.verifyIntent(intent);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        // make sure you call this code before super.onBackPressed();
+        if (otplessView.onBackPressed()) return;
+        super.onBackPressed();
     }
 }
--- otpless_flutter_demo/android/app/src/main/kotlin/com/example/otpless_flutter_demo/MainActivity.kt
+++ otpless_flutter_demo/android/app/src/main/kotlin/com/example/otpless_flutter_demo/MainActivity.kt
@@ -5,19 +5,11 @@ import com.example.otpless_flutter.OtplessFlutterPlugin
 import android.content.Intent
 
 class MainActivity: FlutterActivity() {
-    override fun onNewIntent(intent: Intent) {
-	super.onNewIntent(intent)
-	val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
-	if (plugin is OtplessFlutterPlugin) {
-		plugin.onNewIntent(intent)
-	}
-}
-override fun onBackPressed() {
-	val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
-	if (plugin is OtplessFlutterPlugin) {
-		if (plugin.onBackPressed()) return
-	}
-	// handle other cases
-	super.onBackPressed()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+ super.onActivityResult(requestCode, resultCode, data)
+ val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java) 
+ if (plugin is OtplessFlutterPlugin) {
+  plugin.onActivityResult(requestCode, resultCode, data) 
+ }
 }
 }
