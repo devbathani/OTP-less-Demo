e3c2ae91bf2b50272de5c732fcef14168899f8d4
devbathani <bathanid888@gmail.com>
2023-09-20 16:41:38 +0530

    updates

---
+++ otpless_ios_demo/otpless_ios_demoUITests/otpless_ios_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  otpless_ios_demoUITestsLaunchTests.swift
+//  otpless_ios_demoUITests
+//
+//  Created by Dev Bathani on 19/09/23.
+//
+
+import XCTest
+
+final class otpless_ios_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
